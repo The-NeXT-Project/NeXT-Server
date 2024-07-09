9adeb3b5c480cf9b7d80625b707d46cbbb072c7d
Cat <cat@nextpanel.dev>
2024-07-10 02:31:05 +0800

    feat: add splithttp support

--- cmd/distro/all/all.go
+++ cmd/distro/all/all.go
@@ -31,9 +31,12 @@ import (
 	_ "github.com/xtls/xray-core/proxy/vmess/outbound"
 	// Transports
 	_ "github.com/xtls/xray-core/transport/internet/domainsocket"
+	_ "github.com/xtls/xray-core/transport/internet/grpc"
 	_ "github.com/xtls/xray-core/transport/internet/http"
+	_ "github.com/xtls/xray-core/transport/internet/httpupgrade"
 	_ "github.com/xtls/xray-core/transport/internet/kcp"
 	_ "github.com/xtls/xray-core/transport/internet/quic"
+	_ "github.com/xtls/xray-core/transport/internet/splithttp"
 	_ "github.com/xtls/xray-core/transport/internet/tcp"
 	_ "github.com/xtls/xray-core/transport/internet/tls"
 	_ "github.com/xtls/xray-core/transport/internet/udp"
